79f701d53152b6041e3a58493f304c7419c44cde
DrVrej <DrVrej@users.noreply.github.com>
2020-02-02 19:39:34 -0500

    Test C++

---
+++ C++ Cheat Sheet.cpp
@@ -0,0 +1,5 @@
+//////////////////////// C++ Cheat Sheet \\\\\\\\\\\\\\\\\\\\\\\\
+	///// Created by DrVrej | Since January 30, 2020! \\\\\
+
+/* A macro, basically before full compilation, it gets all the tokens in the code and replaces it with the value. (Kind like find & replace!)  */
+#define token [value]
