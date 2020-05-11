6a319acc314f56372088faf79c2eb86ebd7b45ed
DrVrej <DrVrej@users.noreply.github.com>
2020-05-10 20:38:55 -0400

    CMD Colors + -static
    CMD Colors + -static

--- C++ Cheat Sheet.cpp
+++ C++ Cheat Sheet.cpp
@@ -15,6 +15,7 @@
 * -Wall		= Produce warning messages about a number of things that are legal but unreliable. Suggested to always use this and treat every warning as an error to be fixed.
 * -Wextra	= Enables some extra warning flags that are not enabled by -Wall. More Information: http://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html
 * -I		= Specifies an alternate include directory. Example:  -I/source/headers/  No spaces after the -I command!
+* -static	= Includes all the needed dll files into the exe, making it a semi-independent application
 */
 /*
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -25,6 +26,25 @@
 */
 /*********************** General Notes ***********************/
 /* "\n" is slower then '\n' because it treats it as a string and has to run a loop. */
+
+/* Useful Console Colors */
+#define COLOR_DEFAULT "\033[0m" /* Default */
+#define COLOR_BLACK "\033[30m" /* Black */
+#define COLOR_RED "\033[31m" /* Red */
+#define COLOR_GREEN "\033[32m" /* Green */
+#define COLOR_YELLOW "\033[33m" /* Yellow */
+#define COLOR_BLUE "\033[34m" /* Blue */
+#define COLOR_MAGENTA "\033[35m" /* Magenta */
+#define COLOR_CYAN "\033[36m" /* Cyan */
+#define COLOR_WHITE "\033[37m" /* White */
+#define COLOR_BOLDBLACK "\033[1m\033[30m" /* Bold Black */
+#define COLOR_BOLDRED "\033[1m\033[31m" /* Bold Red */
+#define COLOR_BOLDGREEN "\033[1m\033[32m" /* Bold Green */
+#define COLOR_BOLDYELLOW "\033[1m\033[33m" /* Bold Yellow */
+#define COLOR_BOLDBLUE "\033[1m\033[34m" /* Bold Blue */
+#define COLOR_BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
+#define COLOR_BOLDCYAN "\033[1m\033[36m" /* Bold Cyan */
+#define COLOR_BOLDWHITE "\033[1m\033[37m" /* Bold White */
 
 /* Variable Initialization */
 	type identifier{expression};
